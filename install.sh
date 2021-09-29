f0b802f862d8fb47431b02ef2fdda6a5bf6f1c11
35c4x7hy2ury <91568183+35c4x7hy2ury@users.noreply.github.com>
2021-09-29 20:37:14 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,73 +1,41 @@
-#! /bin/sh -eux
+FROM alpine:edge
 
-echo "Set variables for $(arch)"
+LABEL AUTHOR=Junv<wahyd4@gmail.com>
 
-caddy_version=2.4.1
-filebrowser_version=v2.15.0
-rclone_version=v1.55.0
-ariang_version=1.2.1
+WORKDIR /app
 
-case "$(arch)" in
+ENV RPC_SECRET=""
+ENV ENABLE_AUTH=false
+ENV ENABLE_RCLONE=true
+ENV DOMAIN=:80
+ENV ARIA2_USER=user
+ENV ARIA2_PWD=password
+ENV ARIA2_SSL=false
+ENV ARIA2_EXTERNAL_PORT=80
+ENV PUID=1000
+ENV PGID=1000
+ENV CADDYPATH=/app
+ENV RCLONE_CONFIG=/app/conf/rclone.conf
+ENV XDG_DATA_HOME=/app/.caddy/data
+ENV XDG_CONFIG_HOME=/app/.caddy/config
 
-   x86_64)
-      platform=linux-amd64
-      caddy_file=caddy_${caddy_version}_linux_amd64.tar.gz
-      rclone_file=rclone-${rclone_version}-${platform}.zip
-      forego_file=forego-stable-${platform}.tgz
-     ;;
-   armv7l)
-     platform=linux-armv7
-     caddy_file=caddy_${caddy_version}_linux_armv7.tar.gz
-     rclone_file=rclone-${rclone_version}-linux-arm-v7.zip
-     forego_file=forego-stable-linux-arm.tgz
-     ;;
+ADD install.sh aria2c.sh caddy.sh Procfile init.sh start.sh rclone.sh /app/
+ADD conf /app/conf
+ADD Caddyfile SecureCaddyfile HerokuCaddyfile /usr/local/caddy/
 
-   aarch64)
-     platform=linux-arm64
-     caddy_file=caddy_${caddy_version}_linux_arm64.tar.gz
-     rclone_file=rclone-${rclone_version}-${platform}.zip
-     forego_file=forego-stable-${platform}.tgz
-     ;;
+RUN ./install.sh
 
-   *)
-     echo "unsupported arch $(arch), exit now"
-     exit 1
-     ;;
-esac
+RUN rm ./install.sh
 
-filebrowser_file=${platform}-filebrowser.tar.gz
-ariang_file=AriaNg-${ariang_version}.zip
+# folder for storing ssl keys
+VOLUME /app/conf/key
 
-adduser -D -u 1000 junv \
-  && apk update \
-  && apk add runit shadow wget bash curl openrc gnupg aria2 tar mailcap --no-cache \
-  && wget -N https://github.com/caddyserver/caddy/releases/download/v${caddy_version}/${caddy_file} \
-  && tar -zxf ${caddy_file} \
-  && mv caddy /usr/local/bin/ \
-  && rm -rf ${caddy_file} \
-  && platform=linux-amd64 \
-  && wget -N https://github.com/filebrowser/filebrowser/releases/download/${filebrowser_version}/${filebrowser_file} \
-  && tar -zxf ${filebrowser_file} \
-  && rm -rf ${filebrowser_file} \
-  && rm LICENSE README.md CHANGELOG.md \
-  && wget -N https://bin.equinox.io/c/ekMN3bCZFUn/${forego_file} \
-  && tar -zxf ${forego_file} \
-  && rm -rf ${forego_file} \
-  && mkdir -p /usr/local/www \
-  && mkdir -p /usr/local/www/aria2 \
-  && rm -rf init /app/*.txt \
-  && curl -O https://downloads.rclone.org/${rclone_version}/${rclone_file} \
-  && unzip ${rclone_file} \
-  && cd rclone-* \
-  && cp rclone /usr/local/bin/ \
-  && chown junv:junv /usr/local/bin/rclone \
-  && chmod 755 /usr/local/bin/rclone \
-  && rm /app/${rclone_file} \
-  && rm -rf /app/rclone-* \
-  && mkdir /usr/local/www/aria2/Download \
-  && cd /usr/local/www/aria2 \
-  && chmod +rw /app/conf/aria2.session \
-  && wget -N --no-check-certificate https://github.com/mayswind/AriaNg/releases/download/${ariang_version}/${ariang_file} \
-  && unzip ${ariang_file} \
-  && rm -rf ${ariang_file} \
-  && chmod -R 755 /usr/local/www/aria2 \
+# file downloading folder
+VOLUME /data
+
+EXPOSE 80 443
+
+HEALTHCHECK --interval=1m --timeout=3s \
+  CMD curl -f http://localhost || exit 1
+
+CMD ["./start.sh"]
